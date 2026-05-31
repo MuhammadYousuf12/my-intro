e8537bab8ae7059c9aac8d0110e31f106a86cfc0
MuhammadYousuf12 <muhammad.yousuf01@gmail.com>
2026-06-01 03:54:07 +0500

    chore: remove secrets.dart from repository

--- lib/config/secrets.dart
+++
@@ -1,7 +0,0 @@
-// API credentials - not tracked by Git
-class Secrets {
-  static const String url =
-      'https://api.jsonbin.io/v3/b/6a05f34aadc21f119a9d998e/latest';
-  static const String jsonBinApiKey =
-      r'$2a$10$uNqngPBh9Hb977OzJCuEF.sDN/jQG.FidEJQiFkDEk4CcX6jCXIuW';
-}
